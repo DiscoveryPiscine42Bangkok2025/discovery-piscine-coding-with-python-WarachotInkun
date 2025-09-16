4497769fa309faf03821f6ae4ab9061f2f9fd683
WarachotInkun <warachot.iq@gmail.com>
2025-09-16 23:42:12 +0700

    Add age.py, calculator.py, float.py, and round_up.py scripts for age calculation, basic arithmetic operations, decimal checking, and rounding up numbers

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+a = float(input("Give me the first number: "))
+b = float(input("Give me the second number: "))
+print("Thank you!")
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} / {b} = {a/b}")
+print(f"{a} * {b} = {a*b}")
